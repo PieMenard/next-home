56e2a7654ada055cb0ffe04f0607f92f86dec480
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-28 16:29:37 -0400

    override ant design defaults

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ConfigProvider } from 'antd';
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#1B4242',
+            borderRadius: 2,
+          },
+          components: {
+            Button: {
+              controlHeight: 40,
+              boxShadow: 'none',
+              controlOutline: 'none',
+              colorBorder: '#1B4242',
+            },
+            Input: {
+              controlHeight: 45,
+              boxShadow: 'none',
+              activeShadow: 'none',
+            },
+          },
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </div>
+  );
+};
+
+export default ThemeProvider;
