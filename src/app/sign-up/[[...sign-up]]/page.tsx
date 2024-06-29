6450766ef480ebcc7aa362a74b189e515396482d
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-29 12:59:44 -0400

    style auth pages

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  <div className="flex justify-center items-center h-screen bg-primary">
+    <SignUp />
+  </div>;
 }
